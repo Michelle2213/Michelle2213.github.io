390f1a949bf85a515202ac6021ec695828207ae3
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-08-18 15:56:43 -0500

    Update ejercicio-2.js

--- rv/ejercicio-2.js
+++ rv/ejercicio-2.js
@@ -15,6 +15,8 @@ var forma2= new THREE.SphereGeometry(10,64,64);
 var material2= new THREE.MeshNormalMaterial({color: 0xffff00});
 var esfera= new THREE.Mesh(forma2,material2);
 
+cilindro.rotateX(-Math.PI/4);
+cilindro.rotateY(Math.PI/4);
 
 escena.add(cilindro);
 escena.add(esfera);

9e8fe134f5ab84b0559b35d81ace8ceeee8f8a04
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-08-20 00:52:31 -0500

    Update ejercicio-2.js

--- rv/ejercicio-2.js
+++ rv/ejercicio-2.js
@@ -8,13 +8,15 @@ renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 
 var forma1= new THREE.CylinderGeometry(3,3,30,64);
-var material1= new THREE.MeshLambertMaterial({color: 0x9999FF});
+var material1= new THREE.MeshNormalMaterial();
 var cilindro= new THREE.Mesh(forma1,material1);
 
 var forma2= new THREE.SphereGeometry(10,64,64);
 var material2= new THREE.MeshNormalMaterial();
 var esfera= new THREE.Mesh(forma2,material2);
 
+cilindro.position.set(-1.5, 0.0, -7.0)
 escena.add(cilindro);
+esfera.position.set(-1.5, 0.0, -7.0)
 escena.add(esfera);
 renderizador.render(escena,camara);

a9f5fe7d3819fa1a5cdb4c7aabbd3dd9272d54f0
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-08-23 15:41:29 -0500

    Update ejemplo-geometry.js

--- ejemplo-geometry.js
+++ ejemplo-geometry.js
@@ -23,7 +23,7 @@ malla.rotateX(Math.PI/4);
 var escena = new THREE.Scene();
 escena.add( malla );
 
-var camara = new THREE.PerspectiveCamara();
+var camara = new THREE.PerspectiveCamera();
 camara.position.z= 50;
 
 var renderizador =new THREE.WebGLRenderer();

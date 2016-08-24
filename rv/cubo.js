4193caa81c53342fbff4e0099d96ddde5a1b0837
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-08-23 19:10:08 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -27,8 +27,8 @@ forma.computeFaceNormals(); //Se computan las normales de las caras;
 
 var material= new THREE.MeshNormalMaterial();
 var malla= new THREE.Mesh( forma, material);
-malla.rotateX(Math.PI/2);
-malla.rotateY(Math.PI/2);
+malla.rotateX(Math.PI/4);
+malla.rotateY(Math.PI/4);
 
 var escena = new THREE.Scene();
 escena.add( malla );

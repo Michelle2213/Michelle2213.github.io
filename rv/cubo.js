282a57b7dfed39b19ebb8744554c5dc88e21a295
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-08-23 19:02:13 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -9,7 +9,7 @@ forma.vertices.push( new THREE.Vector3(  1, 1, -1));
 forma.vertices.push( new THREE.Vector3( -1, 1, -1));
 forma.vertices.push( new THREE.Vector3( -1, 1,  1));
 
-forma.faces.push( new THREE.Face3( 3, 2, 1)); //Cara 0
+forma.faces.push( new THREE.Face3( 3, 1, 2)); //Cara 0
 forma.faces.push( new THREE.Face3( 3, 0, 1)); //Cara 1
 //forma.faces.push( new THREE.Face3( 7, 6, 5)); //Cara 2
 //forma.faces.push( new THREE.Face3( 7, 4, 5)); //Cara 3

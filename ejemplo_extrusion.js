6dfd56c80c07828a722d43a2a6f4b8c66d85f2f6
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-08-25 15:35:54 -0500

    Update ejemplo_extrusion.js

--- ejemplo_extrusion.js
+++ ejemplo_extrusion.js
@@ -10,6 +10,7 @@ var forma= new THREE.ExtrudeGeometry( figura, {amount: 10});
 var material= new THREE.MeshNormalMaterial();
 var malla= new THREE.Mesh( forma, material);
 malla.rotateX( Math.PI/4);
+malla.rotateY( Math.PI/4);
 
 var escena= new THREE.Scene();
 escena.add(malla);

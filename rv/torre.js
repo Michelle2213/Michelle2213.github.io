115eaed899abcecb95392e022734b6ab9a0a3506
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-08-31 22:58:04 -0500

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -19,7 +19,7 @@ var basemalla= new THREE.Mesh( baseforma, basematerial);
 basemalla.rotateX(Math.PI/6);
 
 var escena= new THREE.Scene();
-escena.add(malla);
+escena.add(basemalla);
 
 var camara= new THREE.PerspectiveCamera();
 camara.position.z= 500;

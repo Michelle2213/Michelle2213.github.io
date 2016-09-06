5593aa71bafcfd352b6d720d2a6a6922c4dd39a9
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-09-06 15:06:33 -0500

    Create camara-ortografica.js

---
+++ camara-ortografica.js
@@ -0,0 +1,13 @@
+//Camara ortografica (prisma)
+
+var camara= new THREE.OrtographicCamera();
+
+camara.left= window.innerWidth/ -2;
+camara.right= window.innerWidth/ 2;
+camara.top= window.innerHeight/ 2;
+camara.bottom= window.innerHeight/ -2;
+camara.near= 0.1;
+camara.far= 1000;
+camara.updateProjectionMatrix();
+
+camara.position.z=100;

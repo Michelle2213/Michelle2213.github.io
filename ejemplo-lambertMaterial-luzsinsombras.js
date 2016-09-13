f47a643469f824eb4c44070450562db010139cc4
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-09-13 15:43:11 -0500

    Update ejemplo-lambertMaterial-luzsinsombras.js

--- ejemplo-lambertMaterial-luzsinsombras.js
+++ ejemplo-lambertMaterial-luzsinsombras.js
@@ -17,7 +17,7 @@ var camara= new THREE.PerspectiveCamera();
 camara.position.z=15;
 camara.position.y=5;
 
-var lienzo= document.getElementById("luzSinsombras");
+var lienzo= document.getElementById("luzSinSombras");
 var renderizador= new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
 
 renderizador.setSize(600,600);

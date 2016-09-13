06370c2eae4894e9b38fe4db178fe2d96a1779ce
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-09-13 15:08:11 -0500

    Update ejemplo-lambertMaterial.js

--- ejemplo-lambertMaterial.js
+++ ejemplo-lambertMaterial.js
@@ -2,7 +2,7 @@ var forma= new THREE.SphereGeometry(1);
 var material= new THREE.MeshLambertMaterial({color: '#00cc00'});
 var malla= new THREE.Mesh(forma,material);
 
-var escena= newTHREE.Scene();
+var escena= new THREE.Scene();
 escena.add(malla);
 
 var camara= new THREE.PerspectiveCamara();

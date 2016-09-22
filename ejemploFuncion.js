e2c630f1314f773d231b93480ffc8860da615a34
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-09-22 15:03:29 -0500

    Update ejemploFuncion.js

--- ejemploFuncion.js
+++ ejemploFuncion.js
@@ -3,13 +3,15 @@ function init(p){
   escena= new THREE.Scene();
   escena.add(malla);
   camara= new THREE.PerspectiveCamera();
-  renderizador= new THREE.WebGLRender();
+  renderizador= new THREE.WebGLRenderer();
   renderizador.setSize(700,700);
   document.body.appendChild( renderizador.domElement );
-  renderizador.render( escena, camara);
   }
   
 var main= function(p){
   renderizador.render(escena,camara);
   }
   
+var escena, camara, renderizador;
+init(1);
+main(2);

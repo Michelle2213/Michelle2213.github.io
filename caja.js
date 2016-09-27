84c59d6afa61801fd9f9e80f300e219151662d66
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-09-27 15:50:26 -0500

    Update caja.js

--- caja.js
+++ caja.js
@@ -2,19 +2,19 @@ var TEXTURA= new Object();
 
 TEXTURA.retrollamada= function( textura ){
   var material= new THREE.MeshBasicMaterial({map:textura});
-  TEXTURA.malla= new THREE.Mesh(new THREE.SphereGeometry(1), material);
+  TEXTURA.malla= new THREE.Mesh(new THREE.BoxGeometry(1,1,1), material);
   TEXTURA.escena.add(TEXTURA.malla);
  }
  
  TEXTURA.setup= function(){
    TEXTURA.escena= new THREE.Scene();
    var cargador= new THREE.TextureLoader();
-   cargador.load("earth_atmos_2048.jpg", TEXTURA.retrollamada);
+   cargador.load("crate.gif", TEXTURA.retrollamada);
    
    TEXTURA.camara= new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000 );
    TEXTURA.camara.position.z=5;
    
-   var lienzo= document.getElementById("ejemplo-textura");
+   var lienzo= document.getElementById("ejemplo-caja");
    TEXTURA.renderizador= new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
    TEXTURA.renderizador.setSize(600, 600);
   }

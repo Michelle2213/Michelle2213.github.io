f785a723dccba0520a190b71ca31fb74668a2f6a
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-10-17 16:38:43 -0500

    Update tableroJunto.js

--- rv/tableroJunto.js
+++ rv/tableroJunto.js
@@ -188,8 +188,8 @@ CONSTRUCTOR.setup = function(){
       peonesBlancos.scale.z=0.2;
       var material = new THREE.MeshLambertMaterial({map: texturaBlanco});
       var peonesBlancosMalla = new THREE.Mesh(peonesBlancos,material);
-      CONSTRUCTOR.escena.add(peonesBlancosMalla);
-    }
+     }
+       CONSTRUCTOR.escena.add(peonesBlancosMalla);
     }
 
     

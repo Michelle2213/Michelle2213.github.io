1c9a5a7a01a40b9ce345c126a6838fd0a6578e7b
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-12-03 17:55:03 -0600

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -19,7 +19,7 @@ puntos.push( new THREE.Vector2(0,100));
 
 var puntos2=[];
 
-for ( var i = 0; i < 6; i ++ ) {
+for ( var i = 0; i < 15; i ++ ) {
 	puntos2.push( new THREE.Vector2( Math.sin(i*0.2 -40) * 30+20, i));
 }
 

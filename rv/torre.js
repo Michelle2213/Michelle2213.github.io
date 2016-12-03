654af8569853497c4d74c9cceeebdb77a4d89e8b
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-12-03 17:19:54 -0600

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -19,8 +19,8 @@ puntos.push( new THREE.Vector2(0,100));
 
 var puntos2=[];
 
-for ( var i = 0; i < 10; i ++ ) {
-	puntos2.push( new THREE.Vector2( Math.sin( i ) * 10 + 20, ( i - 5 ) * 10 ) );
+for ( var i = 0; i < 6; i ++ ) {
+	puntos2.push( new THREE.Vector2( Math.sin( i + 45 ) * 10, ( i - 5 ) * 10 ) );
 }
 
 var baseAlfil= new THREE.LatheGeometry(puntos);

d16595d886e26f13c63618c2efa60247aa11d18e
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-12-04 17:51:47 -0600

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -35,7 +35,7 @@ var escena= new THREE.Scene();
 escena.add(CaballoMalla);
 
 var camara= new THREE.PerspectiveCamera();
-camara.position.z= 400;
+camara.position.z= 1500;
 
 var renderizador= new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);

c47b67992a99ad990a9622a5b8236028bca87f38
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-12-04 17:49:26 -0600

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -1,53 +1,38 @@
-var puntosreina= [];
+var shape= new THREE.Shape();
 
-    puntosreina.push( new THREE.Vector2(0,0));
-    puntosreina.push( new THREE.Vector2(20,0));
-    puntosreina.push( new THREE.Vector2(20,10));
-    puntosreina.push( new THREE.Vector2(15,10));
-    puntosreina.push( new THREE.Vector2(15,15));
-    puntosreina.push( new THREE.Vector2(10,15));
-    puntosreina.push( new THREE.Vector2(5,60));
-    puntosreina.push( new THREE.Vector2(20,60));
-    puntosreina.push( new THREE.Vector2(20,65));
-    puntosreina.push( new THREE.Vector2(10,65));
-    puntosreina.push( new THREE.Vector2(10,70));
-    puntosreina.push( new THREE.Vector2(15,70));
-    puntosreina.push( new THREE.Vector2(15,80));
-    puntosreina.push( new THREE.Vector2(10,80));
-    puntosreina.push( new THREE.Vector2(20,100));
-    puntosreina.push( new THREE.Vector2(0,100));
+    shape.moveTo(77,0);
+    shape.lineTo(40,32);
+    shape.lineTo(20,205);
+    shape.lineTo(120,402);
+    shape.lineTo(237,412);
+    shape.lineTo(273,450);
+    shape.lineTo(281,384);
+    shape.lineTo(372,250);
+    shape.lineTo(322,206);
+    shape.lineTo(294,234);
+    shape.lineTo(215,223);
+    shape.lineTo(205,263);
+    shape.lineTo(185,261);
+    shape.lineTo(176,224);
+    shape.lineTo(307,97);
+    shape.lineTo(309,30);
+    shape.lineTo(283,0);
+    shape.lineTo(77,0);
 
-var Reina= new THREE.Geometry();
+var perfilCaballo= new THREE.ExtrudeGeometry(shape,50);
+var perfilCaballoMalla= new THREE.Mesh(perfilCaballo);
 
-    for(var i=0; i<=6; i++){
-    var picoForma = new THREE.ConeGeometry( 7, 15 );
-      picoForma.translate(13*(Math.sin(Math.PI*2/6*i)),108,13*(Math.cos(Math.PI*2/6*i)));
-    var picoMalla =new THREE.Mesh(picoForma); 
-    var picoPunta= new THREE.SphereGeometry(3);
-      picoPunta.translate(13*(Math.sin(Math.PI*2/6*i)),116,13*(Math.cos(Math.PI*2/6*i)));
-    var picoPuntaMalla =new THREE.Mesh(picoPunta); 
-      Reina.merge(picoMalla.geometry, picoMalla.matrix);
-      Reina.merge(picoPuntaMalla.geometry, picoPuntaMalla.matrix);
-    }
-
-var baseReina= new THREE.LatheGeometry(puntosreina);
-var baseReinaMalla= new THREE.Mesh(baseReina);
-
-//var puntitaAlfil = new THREE.SphereGeometry( 5 );
-//puntitaAlfil.translate(0,130,0);
-//var puntitaAlfilMalla= new THREE.Mesh(puntitaAlfil);
-
-
-Reina.merge(baseReinaMalla.geometry, baseReinaMalla.matrix);
+var Caballo= new THREE.Geometry();
+Caballo.merge(perfilCaballoMalla.geometry, perfilCaballoMalla.matrix);
 //Alfil.merge(gorroAlfilMalla.geometry, gorroAlfilMalla.matrix);
 //Alfil.merge(puntitaAlfilMalla.geometry, puntitaAlfilMalla.matrix);
 
 var material= new THREE.MeshNormalMaterial();
-var ReinaMalla= new THREE.Mesh(Reina, material);
+var CaballoMalla= new THREE.Mesh(Caballo, material);
 
-ReinaMalla.rotateX(Math.PI/6);
+//CaballoMalla.rotateX(Math.PI/6);
 var escena= new THREE.Scene();
-escena.add(ReinaMalla);
+escena.add(CaballoMalla);
 
 var camara= new THREE.PerspectiveCamera();
 camara.position.z= 400;

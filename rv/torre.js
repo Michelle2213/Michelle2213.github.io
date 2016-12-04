b7bd75e1a44945f99734f13b1dc60f0e429174f7
Michelle2213 <michelle.acevedo2213@gmail.com>
2016-12-04 16:47:24 -0600

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -24,7 +24,7 @@ var Reina= new THREE.Geometry();
       picoForma.translate(13*(Math.sin(Math.PI*2/6*i)),110,13*(Math.cos(Math.PI*2/6*i)));
     var picoMalla =new THREE.Mesh(picoForma); 
     var picoPunta= new THREE.SphereGeometry(3);
-      picoPunta.translate(13*(Math.sin(Math.PI*2/6*i)),125,13*(Math.cos(Math.PI*2/6*i)));
+      picoPunta.translate(13*(Math.sin(Math.PI*2/6*i)),122,13*(Math.cos(Math.PI*2/6*i)));
     var picoPuntaMalla =new THREE.Mesh(picoPunta); 
       Reina.merge(picoMalla.geometry, picoMalla.matrix);
       Reina.merge(picoPuntaMalla.geometry, picoPuntaMalla.matrix);
